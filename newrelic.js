73eac627e6e4866c9c06dda332828245f59e12bc
pedmanuel <ped.manuel.ped@gmail.com>
2023-08-13 01:14:23 +0100

    COMMIT DO ERRO DE INDEX

--- newrelic.js
+++
@@ -1,7 +0,0 @@
-exports.config = {
-    app_name: ['server'],
-    license_key: 'eu01xx5969209d1c695dcdefd80670d3FFFFNRAL',
-    logging: {
-      level: 'info'
-    }
-  };
--- server.js
+++ server.js
@@ -21,31 +21,15 @@ mongoose
   .catch((err) => console.log("ERRO AO CONECTAR BASE DE DADOS", err));
 
 // Middlewares
-app.use(compression());
 app.use(morgan("dev"));
 app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors());
 
-// Rotas do middleware
-
-//ler e sicnronizar as rotas
+// Rotas do middleware ler e sicnronizar as rotas
 readdirSync("./routes").map((r) => app.use("/api", require("./routes/" + r)));
 
-app.use(express.static(path.join(__dirname, "build")));
+// port
 
-// Exemplo de uso do Mongoose para buscar documentos na coleção "ecom-expshoppbd"
-const Documento = mongoose.model("Documento", new mongoose.Schema({})); // Substitua "Documento" pelo nome do modelo desejado
-
-app.get("/buscar-documentos", async (req, res) => {
-  try {
-    const documentos = await Documento.find({});
-    res.json(documentos);
-  } catch (error) {
-    console.error("Erro ao buscar documentos:", error);
-    res.status(500).json({ error: "Erro ao buscar documentos" });
-  }
-});
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, () => console.log(`O servidor está ligado na porta ${port}`));
